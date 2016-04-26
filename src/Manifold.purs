c7786c87f0c206803efb68ea5f8f7dc1ff10e15d
bkonkle <brandon@konkle.us>
2016-04-26 06:09:14 -0600

    Working on simplifying by using channels directly

--- src/Manifold.purs
+++ src/Manifold.purs
@@ -1,4 +1,4 @@
-module Manifold (class Action, AsyncAction, initState, payload) where
+module Manifold where
 
 import Prelude (class Show, Unit, ($), bind, map, pure)
 
@@ -7,9 +7,8 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Exception (EXCEPTION)
 import Data.Foldable (foldl, sequence_)
-import Data.List (List(Nil), (:), fromFoldable, reverse, singleton)
-import Data.Maybe.Unsafe (fromJust)
-import Signal (Signal, (~>), mergeMany, foldp, runSignal)
+import Data.List (List(Nil))
+import Signal (Signal, (~>), foldp, runSignal)
 import Signal.Channel (CHANNEL, Channel, channel, subscribe, send)
 
 class (Show action) <= Action action where
@@ -17,22 +16,13 @@ class (Show action) <= Action action where
 
 type AsyncAction a eff = (Action a) => Aff (channel :: CHANNEL | eff) (List a)
 
-type Config action state eff = (Action action) =>
-  { actions :: Array (Signal action)
-  , effects :: Array (Signal (AsyncAction action eff))
-  , update :: action -> state -> state
-  , initialState :: state }
-
 type CoreEffects eff = (channel :: CHANNEL, err :: EXCEPTION | eff)
 
--- | Merge a list of signals from a channel into a single signal that expects a
--- | list.
-createFeed :: forall action.
-              Channel (List action) ->
-              List (Signal action) ->
-              Signal (List action)
-createFeed chan signals = fromJust $ mergeMany $ reverse $
-  (subscribe chan) : map (map singleton) signals
+type Store action state eff = (Action action) =>
+  { state :: (Signal state)
+  , channels ::
+    { actions :: Channel (List action)
+    , affects :: Channel (List (AsyncAction action eff)) } }
 
 -- | Launch the affect and pipe the resulting list of actions into the given
 -- | channel.
@@ -44,19 +34,28 @@ mapAffect chan affect = launchAff $ do
   actions <- later affect
   liftEff $ send chan actions
 
+store :: forall action state eff. (Action action) =>
+         Signal state ->
+         Channel (List action) ->
+         Channel (List (AsyncAction action eff)) ->
+         Store action state eff
+store stateSignal actionChannel affectChannel =
+  { state: stateSignal
+  , channels:
+    { actions: actionChannel
+    , affects: affectChannel } }
+
 -- | Initialize state management
-initState :: forall action state eff. (Action action) =>
-             Config action state eff ->
-             Eff (CoreEffects eff) (Signal state)
-initState config = do
+createStore :: forall action state eff. (Action action) =>
+             (action -> state -> state) -> state ->
+             Eff (CoreEffects eff) (Store action state eff)
+createStore update initialState = do
   -- | Set up channels for actions and async effects
   actionChannel <- channel Nil
   affectChannel <- channel Nil
-  let foldState state action = config.update action state
+  let foldState state action = update action state
       foldActions actions state = foldl foldState state actions
-      actionSignal = createFeed actionChannel $ fromFoldable config.actions
-      stateSignal = foldp foldActions config.initialState actionSignal
-      affectSignal = subscribe affectChannel
-      effectSignal = affectSignal ~> map (mapAffect actionChannel)
+      stateSignal = foldp foldActions initialState $ subscribe actionChannel
+      effectSignal = (subscribe affectChannel) ~> map (mapAffect actionChannel)
   runSignal $ effectSignal ~> sequence_
-  pure $ stateSignal
+  pure $ store stateSignal actionChannel affectChannel
