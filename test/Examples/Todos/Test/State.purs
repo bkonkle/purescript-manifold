6b060d831e6d2f8d8753ec2817d59c2c69e1a440
bkonkle <brandon@konkle.us>
2016-07-20 18:38:53 -0600

    Test progress

--- test/Examples/Todos/Test/State.purs
+++ test/Examples/Todos/Test/State.purs
@@ -6,76 +6,73 @@ import Control.Monad.Eff.Exception (EXCEPTION)
 import Control.Monad.Eff.Ref (REF)
 import Data.Array ((:))
 import Test.Unit (TIMER, TestSuite, suite, test)
-import Test.Unit.Assert (equal)
+import Test.Unit.Assert as Assert
 
 import Test.Examples.Todos.State.Todos (Action(..), State(..), Todo(..), update)
 
 todosExampleTestState :: forall eff. TestSuite
   ( err :: EXCEPTION, avar :: AVAR, timer :: TIMER, ref :: REF | eff )
-todosExampleTestState = suite "Examples.Todos.State" do
-  suite "Todos" do
+todosExampleTestState = suite "Test.Examples.Todos.State.Todos" do
+  suite "update" do
+    test "handles Add actions" do
+      let initialState = State
+            { todos: [ Todo { text: "Use Redux", id: 0, completed: false } ]
+            , lastId: 0 }
+          todo = Todo { text: "Become cool", id: 1, completed: false }
+          expected = initialState #
+            \(State state) -> State
+              state { todos = todo : state.todos, lastId = 1 }
+          result = update (Add "Become cool") initialState
+      Assert.equal expected result
 
-    suite "update" do
+    test "handles Delete actions" do
+      let initialState = State
+            { todos: [ Todo { text: "Use Redux", id: 0, completed: false } ]
+            , lastId: 0 }
+          expected = State { todos: [], lastId: 0 }
+          result = update (Delete 0) initialState
+      Assert.equal expected result
 
-      test "handles Add actions" do
-        let initialState = State
-              { todos: [ Todo { text: "Use Redux", id: 0, completed: false } ]
-              , lastId: 0 }
-            todo = Todo { text: "Become cool", id: 1, completed: false }
-            expected = initialState #
-              \(State state) -> State
-                state { todos = todo : state.todos, lastId = 1 }
-            result = update (Add "Become cool") initialState
-        result `equal` expected
+    test "handles Edit actions" do
+      let initialState = State
+            { todos: [ Todo { text: "Use Redux", id: 0, completed: false } ]
+            , lastId: 0 }
+          expected = State
+            { todos: [ Todo { text: "Use PureScript", id: 0, completed: false } ]
+            , lastId: 0 }
+          result = update (Edit 0 "Use PureScript") initialState
+      Assert.equal expected result
 
-      test "handles Delete actions" do
-        let initialState = State
-              { todos: [ Todo { text: "Use Redux", id: 0, completed: false } ]
-              , lastId: 0 }
-            expected = State { todos: [], lastId: 0 }
-            result = update (Delete 0) initialState
-        result `equal` expected
+    test "handles Complete actions" do
+      let initialState = State
+            { todos: [ Todo { text: "Use Redux", id: 0, completed: false } ]
+            , lastId: 0 }
+          expected = State
+            { todos: [ Todo { text: "Use Redux", id: 0, completed: true } ]
+            , lastId: 0 }
+          result = update (Complete 0 true) initialState
+      Assert.equal expected result
 
-      test "handles Edit actions" do
-        let initialState = State
-              { todos: [ Todo { text: "Use Redux", id: 0, completed: false } ]
-              , lastId: 0 }
-            expected = State
-              { todos: [ Todo { text: "Use PureScript", id: 0, completed: false } ]
-              , lastId: 0 }
-            result = update (Edit 0 "Use PureScript") initialState
-        result `equal` expected
+    test "handles CompleteAll actions" do
+      let initialState = State
+            { todos:
+              [ Todo { text: "Use Redux", id: 0, completed: false }
+              , Todo { text: "With PureScript", id: 0, completed: false } ]
+            , lastId: 0 }
+          expected = State
+            { todos:
+              [ Todo { text: "Use Redux", id: 0, completed: true }
+              , Todo { text: "With PureScript", id: 0, completed: true } ]
+            , lastId: 0 }
+          result = update CompleteAll initialState
+      Assert.equal expected result
 
-      test "handles Complete actions" do
-        let initialState = State
-              { todos: [ Todo { text: "Use Redux", id: 0, completed: false } ]
-              , lastId: 0 }
-            expected = State
-              { todos: [ Todo { text: "Use Redux", id: 0, completed: true } ]
-              , lastId: 0 }
-            result = update (Complete 0 true) initialState
-        result `equal` expected
-
-      test "handles CompleteAll actions" do
-        let initialState = State
-              { todos:
-                [ Todo { text: "Use Redux", id: 0, completed: false }
-                , Todo { text: "With PureScript", id: 0, completed: false } ]
-              , lastId: 0 }
-            expected = State
-              { todos:
-                [ Todo { text: "Use Redux", id: 0, completed: true }
-                , Todo { text: "With PureScript", id: 0, completed: true } ]
-              , lastId: 0 }
-            result = update CompleteAll initialState
-        result `equal` expected
-
-      test "handles ClearComplete actions" do
-        let initialState = State
-              { todos:
-                [ Todo { text: "Use Redux", id: 0, completed: true }
-                , Todo { text: "With PureScript", id: 0, completed: true } ]
-              , lastId: 0 }
-            expected = State { todos: [], lastId: 0 }
-            result = update ClearComplete initialState
-        result `equal` expected
+    test "handles ClearComplete actions" do
+      let initialState = State
+            { todos:
+              [ Todo { text: "Use Redux", id: 0, completed: true }
+              , Todo { text: "With PureScript", id: 0, completed: true } ]
+            , lastId: 0 }
+          expected = State { todos: [], lastId: 0 }
+          result = update ClearComplete initialState
+      Assert.equal expected result
